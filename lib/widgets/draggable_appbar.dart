375a43c44569c59262079b3a5ce17bedcce20c1b
Yesterday17 <mmf@mmf.moe>
2021-12-14 01:12:13 +0800

    PlatformDraggableAppbar

--- lib/widgets/draggable_appbar.dart
+++ lib/widgets/draggable_appbar.dart
@@ -1,23 +1,104 @@
 import 'package:bitsdojo_window/bitsdojo_window.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 
-class DraggableAppBar extends StatefulWidget implements PreferredSizeWidget {
-  final Widget appBar;
-
-  const DraggableAppBar({Key? key, required this.appBar}) : super(key: key);
-
-  @override
-  _DraggableAppBarState createState() => _DraggableAppBarState();
-
-  @override
-  Size get preferredSize => new Size.fromHeight(kToolbarHeight);
-}
-
-class _DraggableAppBarState extends State<DraggableAppBar> {
+class _MoveWindow extends StatelessWidget {
+  _MoveWindow({Key? key, this.child}) : super(key: key);
+  final Widget? child;
   @override
   Widget build(BuildContext context) {
-    return MoveWindow(
-      child: widget.appBar,
+    return GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onPanStart: (details) {
+          appWindow.startDragging();
+        },
+        onDoubleTap: () => appWindow.maximizeOrRestore(),
+        child: this.child ?? Container());
+  }
+}
+
+class PreferSizedMoveWindow extends StatelessWidget
+    implements PreferredSizeWidget {
+  final PreferredSizeWidget child;
+  const PreferSizedMoveWindow({Key? key, required this.child})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return _MoveWindow(
+      child: child,
+    );
+  }
+
+  @override
+  Size get preferredSize => child.preferredSize;
+}
+
+class DraggableAppBar extends PlatformAppBar {
+  final Key? widgetKey;
+
+  final Widget? title;
+  final Color? backgroundColor;
+  final Widget? leading;
+  final List<Widget>? trailingActions;
+  final bool? automaticallyImplyLeading;
+
+  DraggableAppBar({
+    Key? key,
+    this.widgetKey,
+    this.title,
+    this.backgroundColor,
+    this.leading,
+    this.trailingActions,
+    this.automaticallyImplyLeading,
+  }) : super(
+          key: key,
+          widgetKey: widgetKey,
+          title: title,
+          backgroundColor: backgroundColor,
+          leading: leading,
+          trailingActions: trailingActions,
+          automaticallyImplyLeading: automaticallyImplyLeading,
+        );
+
+  @override
+  PreferredSizeWidget createMaterialWidget(BuildContext context) {
+    return PreferSizedMoveWindow(child: super.createMaterialWidget(context));
+  }
+
+  @override
+  CupertinoNavigationBar createCupertinoWidget(BuildContext context) {
+    var child = super.createCupertinoWidget(context);
+    return DraggableCupertinoNavigationBar(
+      child: child,
+    );
+  }
+}
+
+class DraggableCupertinoNavigationBar extends CupertinoNavigationBar {
+  final CupertinoNavigationBar child;
+  const DraggableCupertinoNavigationBar({Key? key, required this.child})
+      : super(key: key);
+
+  @override
+  Size get preferredSize => child.preferredSize;
+
+  @override
+  bool shouldFullyObstruct(BuildContext context) =>
+      child.shouldFullyObstruct(context);
+
+  @override
+  State<DraggableCupertinoNavigationBar> createState() =>
+      _DraggableCupertinoNavigationBarState();
+}
+
+class _DraggableCupertinoNavigationBarState
+    extends State<DraggableCupertinoNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return _MoveWindow(
+      child: widget.child,
     );
   }
 }
