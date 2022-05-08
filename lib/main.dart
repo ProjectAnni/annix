4e3f6832863891647158a9ac40ba7cdc14dfc252
Yesterday17 <mmf@mmf.moe>
2022-05-09 00:58:04 +0800

    Disable flutter_ume

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:annix/app.dart';
 import 'package:annix/services/global.dart';
 import 'package:annix/services/platform.dart';
-import 'package:flutter/foundation.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter_ume/flutter_ume.dart';
-import 'package:flutter_ume_kit_ui/flutter_ume_kit_ui.dart';
-import 'package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart';
-import 'package:flutter_ume_kit_show_code/flutter_ume_kit_show_code.dart';
-import 'package:flutter_ume_kit_device/flutter_ume_kit_device.dart';
-import 'package:flutter_ume_kit_console/flutter_ume_kit_console.dart';
+// import 'package:flutter_ume/flutter_ume.dart';
+// import 'package:flutter_ume_kit_ui/flutter_ume_kit_ui.dart';
+// import 'package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart';
+// import 'package:flutter_ume_kit_show_code/flutter_ume_kit_show_code.dart';
+// import 'package:flutter_ume_kit_device/flutter_ume_kit_device.dart';
+// import 'package:flutter_ume_kit_console/flutter_ume_kit_console.dart';
 import 'package:annix/third_party/just_audio_background/just_audio_background.dart';
 import 'package:get/get.dart';
 
@@ -28,22 +28,22 @@ Future<void> main() async {
 
   await Global.init();
 
-  if (kDebugMode) {
-    PluginManager.instance
-      ..register(WidgetInfoInspector())
-      ..register(WidgetDetailInspector())
-      ..register(ColorSucker())
-      ..register(AlignRuler())
-      ..register(ColorPicker())
-      ..register(TouchIndicator())
-      ..register(Performance())
-      ..register(ShowCode())
-      ..register(MemoryInfoPage())
-      ..register(CpuInfoPage())
-      ..register(DeviceInfoPanel())
-      ..register(Console());
-    runApp(UMEWidget(child: AnnixApp(), enable: true));
-  } else {
-    runApp(AnnixApp());
-  }
+  // if (kDebugMode) {
+  //   PluginManager.instance
+  //     ..register(WidgetInfoInspector())
+  //     ..register(WidgetDetailInspector())
+  //     ..register(ColorSucker())
+  //     ..register(AlignRuler())
+  //     ..register(ColorPicker())
+  //     ..register(TouchIndicator())
+  //     ..register(Performance())
+  //     ..register(ShowCode())
+  //     ..register(MemoryInfoPage())
+  //     ..register(CpuInfoPage())
+  //     ..register(DeviceInfoPanel())
+  //     ..register(Console());
+  //   runApp(UMEWidget(child: AnnixApp(), enable: true));
+  // } else {
+  runApp(AnnixApp());
+  // }
 }
