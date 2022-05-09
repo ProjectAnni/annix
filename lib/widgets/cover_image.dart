c9b860b49e4b401c4d984a169e2c0b8eacda58fe
Yesterday17 <mmf@mmf.moe>
2022-05-09 17:02:17 +0800

    ExtendedImage for optimization

--- lib/widgets/cover_image.dart
+++ lib/widgets/cover_image.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:annix/services/global.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 import 'package:synchronized/synchronized.dart';
@@ -29,7 +30,7 @@ class CoverImage extends StatelessWidget {
     this.filterQuality = FilterQuality.low,
   }) : super(key: key);
 
-  Future<Uint8List> getCoverImage() async {
+  Future<File> getCoverImage() async {
     final coverImagePath = getCoverCachePath(albumId, discId);
     final file = File(coverImagePath);
     if (!await file.exists()) {
@@ -48,122 +49,44 @@ class CoverImage extends StatelessWidget {
         // response stream to Uint8List
         final data = (await response.toList()).expand((e) => e).toList();
         final raw = Uint8List.fromList(data);
-        LRUImageCache.instance.cacheData(raw, "$albumId/$discId");
         await file.writeAsBytes(raw);
-        return raw;
       } else {
         throw Exception("Failed to fetch cover image");
       }
-    } else {
-      // local file exists, load to cache
-      final raw = await file.readAsBytes();
-      LRUImageCache.instance.cacheData(raw, "$albumId/$discId");
-      return raw;
     }
+    return file;
   }
 
   @override
   Widget build(BuildContext context) {
-    final key = "$albumId/$discId";
-    if (LRUImageCache.instance.contains(key)) {
-      return Image.memory(
-        LRUImageCache.instance.get(key)!,
-        fit: fit,
-        filterQuality: filterQuality,
-      );
-    } else {
-      return FutureBuilder<Uint8List>(
-        future: getCoverImage(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return Image.memory(
-              snapshot.data!,
-              gaplessPlayback: true,
-              fit: fit,
-              filterQuality: filterQuality,
-            );
-          } else if (snapshot.hasError) {
-            // TODO: show error
-            return Container(
-              child: Center(
-                child: Text(
-                  snapshot.error.toString(),
-                ),
+    return FutureBuilder<File>(
+      future: getCoverImage(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ExtendedImage.file(
+            snapshot.data!,
+            fit: fit,
+            filterQuality: filterQuality,
+            compressionRatio: 0.5,
+          );
+        } else if (snapshot.hasError) {
+          // TODO: show error
+          return Container(
+            child: Center(
+              child: Text(
+                snapshot.error.toString(),
               ),
-            );
-          } else {
-            // loading
-            return Center(
-              child: CircularProgressIndicator(
-                strokeWidth: 2.0,
-              ),
-            );
-          }
-        },
-      );
-    }
-  }
-}
-
-// Modified from [https://github.com/DragonCherry/lru_image_cache]
-//
-// lru_image_cache is available under the MIT license. See the LICENSE file for more info.
-//
-// MIT License
-//
-// Copyright (c) 2020 DragonCherry
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-class LRUImageCache {
-  static final instance = LRUImageCache();
-
-  final _lock = Lock();
-  final _sizeCache = Map<String, Size>();
-  final _identifiers = <String>[];
-  final _map = Map<String, Uint8List>();
-
-  int maximumBytes = 64 * 1024 * 1024; // default 64MB
-  int currentBytes = 0;
-
-  void cacheData(final Uint8List imageBytes, final String key) {
-    _lock.synchronized(() {
-      if (currentBytes + imageBytes.length <= maximumBytes) {
-        currentBytes += imageBytes.length;
-      } else {
-        do {
-          if (_identifiers.isNotEmpty) {
-            final lastIdentifier = _identifiers.removeLast();
-            final imageBytesToRemove = _map.remove(lastIdentifier);
-            currentBytes -= imageBytesToRemove?.length ?? 0;
-          } else {
-            break;
-          }
-        } while (currentBytes + imageBytes.length >= maximumBytes);
-      }
-      _map[key] = imageBytes;
-      _identifiers.insert(0, key);
-    });
-  }
-
-  Size? size(final String identifier) {
-    return _sizeCache[identifier];
-  }
-
-  bool contains(final String url) {
-    return _identifiers.contains(url);
-  }
-
-  Uint8List? get(final String identifier) {
-    return _map[identifier];
-  }
-
-  void clear() {
-    _sizeCache.clear();
-    _identifiers.clear();
-    _map.clear();
+            ),
+          );
+        } else {
+          // loading
+          return Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 2.0,
+            ),
+          );
+        }
+      },
+    );
   }
 }
