a7c7f1d7266712ab584c1dadd0425ef5063204db
Yesterday17 <mmf@mmf.moe>
2022-05-13 11:50:03 +0800

    Remove AnniPlatform

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
+import 'dart:io';
+
 import 'package:annix/app.dart';
 import 'package:annix/controllers/annil_controller.dart';
 import 'package:annix/controllers/anniv_controller.dart';
 import 'package:annix/controllers/network_controller.dart';
 import 'package:annix/controllers/playing_controller.dart';
 import 'package:annix/services/global.dart';
-import 'package:annix/services/platform.dart';
 import 'package:f_logs/f_logs.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,7 @@ import 'package:get/get.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  if (AnniPlatform.isMobile || AnniPlatform.isMacOS) {
+  if (Platform.isAndroid || Platform.isIOS || Platform.isMacOS) {
     await JustAudioBackground.init(
       androidNotificationChannelId: 'rs.anni.annix.audio',
       androidNotificationChannelName: 'Annix Audio playback',
