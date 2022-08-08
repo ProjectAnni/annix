6a756601a9ceb128dbe299c881e45764c2f74d35
Yesterday17 <mmf@mmf.moe>
2022-08-08 13:34:07 +0000

    add missing files

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,23 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <audioplayers_windows/audioplayers_windows_plugin.h>
+#include <bitsdojo_window_windows/bitsdojo_window_plugin.h>
+#include <connectivity_plus_windows/connectivity_plus_windows_plugin.h>
+#include <flutter_lyric/flutter_lyric_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AudioplayersWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
+  BitsdojoWindowPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BitsdojoWindowPlugin"));
+  ConnectivityPlusWindowsPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ConnectivityPlusWindowsPlugin"));
+  FlutterLyricPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FlutterLyricPlugin"));
+}
