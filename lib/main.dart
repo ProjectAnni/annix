30961d49cda272e3560393fcc3cbba569cbf57ac
Yesterday17 <mmf@mmf.moe>
2022-10-07 20:58:10 +0800

    global error handler

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:annix/app.dart';
 import 'package:annix/global.dart';
 import 'package:annix/i18n/strings.g.dart';
 import 'package:f_logs/f_logs.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -10,6 +11,18 @@ Future<void> main() async {
 
   await Global.init();
   LocaleSettings.useDeviceLocale();
+
+  FlutterError.onError = (details) {
+    FLog.error(
+      text: 'Flutter error',
+      exception: details,
+      stacktrace: details.stack,
+    );
+  };
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FLog.error(text: 'Root isolate error', exception: error, stacktrace: stack);
+    return true;
+  };
 
   try {
     runApp(TranslationProvider(child: const AnnixApp()));
