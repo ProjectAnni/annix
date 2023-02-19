6befe369c425ca6f59fec637d8e3a7ac50217fa8
Yesterday17 <mmf@mmf.moe>
2023-02-19 21:44:11 +0800

    fix animation

--- lib/ui/route/page.dart
+++ lib/ui/route/page.dart
@@ -1,6 +1,6 @@
 import 'package:animations/animations.dart';
-import 'package:annix/global.dart';
 import 'package:annix/ui/route/route.dart';
+import 'package:annix/utils/context_extension.dart';
 import 'package:flutter/material.dart';
 
 typedef AnnixRoutePageBuilder = Widget Function(
@@ -60,7 +60,7 @@ class AnnixPage extends Page {
           transitionDuration ?? const Duration(milliseconds: 300),
       pageBuilder: (context, animation, secondaryAnimation) {
         return (pageBuilder ??
-            (Global.isDesktop
+            (context.isDesktopOrLandscape
                 ? fadeTransitionBuilder
                 : fadeThroughTransitionBuilder))(
           context,
