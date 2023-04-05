c46f4c33b27d1573686a4a0e385b7810068d36ba
Yesterday17 <mmf@mmf.moe>
2023-04-06 01:58:15 +0800

    Only cover part in BottomPlayerDesktop can trigger onClick now

--- lib/ui/bottom_player/bottom_player_desktop.dart
+++ lib/ui/bottom_player/bottom_player_desktop.dart
@@ -4,6 +4,7 @@ import 'package:annix/ui/widgets/artist_text.dart';
 import 'package:annix/ui/widgets/buttons/favorite_button.dart';
 import 'package:annix/ui/widgets/buttons/loop_mode_button.dart';
 import 'package:annix/ui/widgets/buttons/play_pause_button.dart';
+import 'package:annix/ui/widgets/volume.dart';
 import 'package:annix/utils/context_extension.dart';
 import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
 import 'package:flutter/material.dart';
@@ -18,111 +19,109 @@ class DesktopBottomPlayer extends ConsumerWidget {
   Widget build(final BuildContext context, final WidgetRef ref) {
     final player = ref.read(playbackProvider);
 
-    return GestureDetector(
-      onTap: () {
-        if (player.playing != null) {
-          onClick();
-        }
-      },
-      child: Container(
-        color: ElevationOverlay.colorWithOverlay(
-          context.colorScheme.surface,
-          context.colorScheme.primary,
-          0.5,
-        ),
-        height: 96,
-        child: Column(
-          children: [
-            // const Divider(height: 1),
-            Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // left
-                  Expanded(
-                    child: Row(
-                      children: [
-                        // cover
-                        const PlayingMusicCover(
+    return Container(
+      color: ElevationOverlay.colorWithOverlay(
+        context.colorScheme.surface,
+        context.colorScheme.primary,
+        0.5,
+      ),
+      height: 96,
+      child: Column(
+        children: [
+          // const Divider(height: 1),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // left
+                Expanded(
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          if (player.playing != null) {
+                            onClick();
+                          }
+                        },
+                        child: const PlayingMusicCover(
                           animated: false,
                           card: false,
                         ),
-                        // track info
-                        Expanded(
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 16),
-                            child: Consumer(
-                              builder: (final context, final ref, final child) {
-                                final playing = ref.watch(playingProvider);
+                      ),
+                      // track info
+                      Expanded(
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Consumer(
+                            builder: (final context, final ref, final child) {
+                              final playing = ref.watch(playingProvider);
 
-                                return Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      playing?.track.title ?? 'Not playing',
-                                      style: context.textTheme.titleMedium
-                                          ?.apply(fontWeightDelta: 2),
-                                      overflow: TextOverflow.ellipsis,
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    playing?.track.title ?? 'Not playing',
+                                    style: context.textTheme.titleMedium
+                                        ?.apply(fontWeightDelta: 2),
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                  ArtistText(
+                                    playing?.track.artist ?? '',
+                                    style: context.textTheme.labelSmall,
+                                  ),
+                                  const SizedBox(height: 8),
+                                  RepaintBoundary(
+                                    child: ProgressBar(
+                                      progress:
+                                          playing?.position ?? Duration.zero,
+                                      total: playing?.duration ?? Duration.zero,
+                                      onSeek: player.seek,
+                                      barHeight: 3.0,
+                                      thumbRadius: 6,
+                                      thumbGlowRadius: 12,
+                                      thumbCanPaintOutsideBar: false,
+                                      timeLabelLocation:
+                                          TimeLabelLocation.sides,
+                                      timeLabelTextStyle:
+                                          context.textTheme.labelSmall,
                                     ),
-                                    ArtistText(
-                                      playing?.track.artist ?? '',
-                                      style: context.textTheme.labelSmall,
-                                    ),
-                                    const SizedBox(height: 8),
-                                    RepaintBoundary(
-                                      child: ProgressBar(
-                                        progress:
-                                            playing?.position ?? Duration.zero,
-                                        total:
-                                            playing?.duration ?? Duration.zero,
-                                        onSeek: player.seek,
-                                        barHeight: 3.0,
-                                        thumbRadius: 6,
-                                        thumbGlowRadius: 12,
-                                        thumbCanPaintOutsideBar: false,
-                                        timeLabelLocation:
-                                            TimeLabelLocation.sides,
-                                        timeLabelTextStyle:
-                                            context.textTheme.labelSmall,
-                                      ),
-                                    ),
-                                  ],
-                                );
-                              },
-                            ),
+                                  ),
+                                ],
+                              );
+                            },
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  // right
-                  Row(
-                    children: [
-                      // const VolumeController(),
-                      const FavoriteButton(),
-                      IconButton(
-                        icon: const Icon(Icons.skip_previous),
-                        iconSize: 28,
-                        onPressed: player.previous,
                       ),
-                      const PlayPauseButton(
-                        type: PlayPauseButtonType.elevated,
-                        size: 56,
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.skip_next),
-                        iconSize: 28,
-                        onPressed: player.next,
-                      ),
-                      const LoopModeButton(),
                     ],
                   ),
-                ],
-              ),
+                ),
+                // right
+                Row(
+                  children: [
+                    const VolumeController(),
+                    const FavoriteButton(),
+                    IconButton(
+                      icon: const Icon(Icons.skip_previous),
+                      iconSize: 28,
+                      onPressed: player.previous,
+                    ),
+                    const PlayPauseButton(
+                      type: PlayPauseButtonType.elevated,
+                      size: 56,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.skip_next),
+                      iconSize: 28,
+                      onPressed: player.next,
+                    ),
+                    const LoopModeButton(),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
