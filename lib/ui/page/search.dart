23cce56f152fa0fcf7b58ec0318263f66dfb5628
Yesterday17 <mmf@mmf.moe>
2024-04-22 09:35:02 +0800

    refactor: optimize search page a bit

--- lib/ui/page/search.dart
+++ lib/ui/page/search.dart
@@ -7,158 +7,11 @@ import 'package:annix/services/settings.dart';
 import 'package:annix/ui/widgets/album/album_wall.dart';
 import 'package:annix/ui/widgets/cover.dart';
 import 'package:annix/ui/widgets/artist_text.dart';
+import 'package:annix/ui/widgets/fade_indexed_stack.dart';
 import 'package:flutter/material.dart';
 import 'package:annix/i18n/strings.g.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-class _SearchResultWidget extends HookConsumerWidget {
-  final ValueNotifier<SearchResult?> result;
-  final ValueNotifier<bool> isLoading;
-
-  const _SearchResultWidget({
-    super.key,
-    required this.result,
-    required this.isLoading,
-  });
-
-  @override
-  Widget build(BuildContext context, final WidgetRef ref) {
-    final categoryState = useState(0);
-
-    if (isLoading.value) {
-      return const Center(
-        child: CircularProgressIndicator(strokeWidth: 2),
-      );
-    }
-
-    if (result.value == null) {
-      return const Center(child: Text('Search results would display here'));
-    }
-
-    return Column(
-      children: [
-        ButtonBar(
-          alignment: MainAxisAlignment.start,
-          children: [
-            if ((result.value?.tracks?.length ?? 0) > 0)
-              FilterChip(
-                label: Text(t.track),
-                onSelected: (selected) {
-                  if (selected) {
-                    categoryState.value = 0;
-                  }
-                },
-                selected: categoryState.value == 0,
-              ),
-            if ((result.value?.albums?.length ?? 0) > 0)
-              FilterChip(
-                label: Text(t.albums),
-                onSelected: (selected) {
-                  if (selected) {
-                    categoryState.value = 1;
-                  }
-                },
-                selected: categoryState.value == 1,
-              ),
-            if ((result.value?.playlists?.length ?? 0) > 0)
-              FilterChip(
-                label: Text(t.playlists),
-                onSelected: (selected) {
-                  if (selected) {
-                    categoryState.value = 2;
-                  }
-                },
-                selected: categoryState.value == 2,
-              ),
-          ],
-        ),
-        if (categoryState.value == 0)
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (final context, final index) {
-                final e = result.value!.tracks![index];
-
-                return ValueListenableBuilder<SearchTrackDisplayType>(
-                  valueListenable:
-                      ref.read(settingsProvider).searchTrackDisplayType,
-                  builder: (final context, final type, final _) {
-                    return ListTile(
-                      isThreeLine: type.isThreeLine,
-                      leading: CoverCard(
-                          child: MusicCover.fromAlbum(albumId: e.id.albumId)),
-                      title: Text(
-                        e.title,
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          if (type.showArtist) ArtistText(e.artist),
-                          if (type.showAlbumTitle)
-                            Text(
-                              e.albumTitle,
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                        ],
-                      ),
-                      onTap: () async {
-                        final player = ref.read(playbackProvider);
-                        final metadata = ref.read(metadataProvider);
-                        final audio = await AnnilAudioSource.from(
-                          id: e.id,
-                          metadata: metadata,
-                        );
-                        if (audio != null) {
-                          await player.setPlayingQueue([audio]);
-                        }
-                      },
-                    );
-                  },
-                );
-              },
-              itemCount: result.value!.tracks?.length ?? 0,
-            ),
-          ),
-        if (categoryState.value == 1)
-          Expanded(
-            child: AlbumWall(
-              albumIds:
-                  result.value!.albums?.map((e) => e.albumId).toList() ?? [],
-            ),
-          ),
-        if (categoryState.value == 2)
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (final context, final index) {
-                final item = result.value!.playlists![index];
-                final coverAlbumId = item.cover?.albumId;
-                return ListTile(
-                  leading: coverAlbumId != null
-                      ? MusicCover.fromAlbum(albumId: coverAlbumId)
-                      : const DummyMusicCover(),
-                  title: Text(
-                    item.name,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  subtitle: Text(item.owner),
-                  onTap: () async {
-                    // TODO: get playlist and save
-                    // final delegate = AnnixRouterDelegate.of(context);
-                    // delegate.to(name: '/playlist', arguments: item.id);
-                  },
-                );
-              },
-              itemCount: result.value!.playlists?.length ?? 0,
-            ),
-          ),
-      ],
-    );
-  }
-}
 
 class SearchPage extends HookConsumerWidget {
   const SearchPage({super.key});
@@ -191,25 +44,181 @@ class SearchPage extends HookConsumerWidget {
     }
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Search')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              // autofocus: true,
-              decoration: InputDecoration(hintText: t.search),
-              onSubmitted: (final keyword) => search(anniv.client!, keyword),
-            ),
-            Expanded(
-              child: _SearchResultWidget(
-                isLoading: isLoadingState,
-                result: resultState,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              TextField(
+                // autofocus: true,
+                decoration: InputDecoration(
+                  icon: const Icon(Icons.search),
+                  hintText: t.search,
+                ),
+                onSubmitted: (final keyword) => search(anniv.client!, keyword),
               ),
-            ),
-          ],
+              Expanded(
+                child: _SearchResultWidget(
+                  isLoading: isLoadingState,
+                  result: resultState,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class _SearchResultWidget extends HookConsumerWidget {
+  final ValueNotifier<SearchResult?> result;
+  final ValueNotifier<bool> isLoading;
+
+  const _SearchResultWidget({
+    super.key,
+    required this.result,
+    required this.isLoading,
+  });
+
+  @override
+  Widget build(BuildContext context, final WidgetRef ref) {
+    final categoryState = useState(0);
+
+    if (isLoading.value) {
+      return const Center(
+        child: CircularProgressIndicator(strokeWidth: 2),
+      );
+    }
+
+    if (result.value == null) {
+      return const Center(child: Text('Search results would display here'));
+    }
+
+    return Column(
+      children: [
+        ButtonBar(
+          alignment: MainAxisAlignment.start,
+          children: [
+            if ((result.value?.tracks?.length ?? 0) > 0)
+              FilterChip(
+                avatar: categoryState.value == 0
+                    ? null
+                    : const Icon(Icons.music_note),
+                label: Text(t.track),
+                onSelected: (selected) {
+                  if (selected) {
+                    categoryState.value = 0;
+                  }
+                },
+                selected: categoryState.value == 0,
+              ),
+            if ((result.value?.albums?.length ?? 0) > 0)
+              FilterChip(
+                avatar: categoryState.value == 1
+                    ? null
+                    : const Icon(Icons.album_outlined),
+                label: Text(t.albums),
+                onSelected: (selected) {
+                  if (selected) {
+                    categoryState.value = 1;
+                  }
+                },
+                selected: categoryState.value == 1,
+              ),
+            if ((result.value?.playlists?.length ?? 0) > 0)
+              FilterChip(
+                label: Text(t.playlists),
+                onSelected: (selected) {
+                  if (selected) {
+                    categoryState.value = 2;
+                  }
+                },
+                selected: categoryState.value == 2,
+              ),
+          ],
+        ),
+        Expanded(
+          child: FadeIndexedStack(
+            index: categoryState.value,
+            children: [
+              ListView.builder(
+                itemBuilder: (final context, final index) {
+                  final e = result.value!.tracks![index];
+
+                  return ValueListenableBuilder<SearchTrackDisplayType>(
+                    valueListenable:
+                        ref.read(settingsProvider).searchTrackDisplayType,
+                    builder: (final context, final type, final _) {
+                      return ListTile(
+                        isThreeLine: type.isThreeLine,
+                        leading: CoverCard(
+                            child: MusicCover.fromAlbum(albumId: e.id.albumId)),
+                        title: Text(
+                          e.title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            if (type.showArtist) ArtistText(e.artist),
+                            if (type.showAlbumTitle)
+                              Text(
+                                e.albumTitle,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                          ],
+                        ),
+                        onTap: () async {
+                          final player = ref.read(playbackProvider);
+                          final metadata = ref.read(metadataProvider);
+                          final audio = await AnnilAudioSource.from(
+                            id: e.id,
+                            metadata: metadata,
+                          );
+                          if (audio != null) {
+                            await player.setPlayingQueue([audio]);
+                          }
+                        },
+                      );
+                    },
+                  );
+                },
+                itemCount: result.value!.tracks?.length ?? 0,
+              ),
+              AlbumWall(
+                albumIds:
+                    result.value!.albums?.map((e) => e.albumId).toList() ?? [],
+              ),
+              ListView.builder(
+                itemBuilder: (final context, final index) {
+                  final item = result.value!.playlists![index];
+                  final coverAlbumId = item.cover?.albumId;
+                  return ListTile(
+                    leading: coverAlbumId != null
+                        ? MusicCover.fromAlbum(albumId: coverAlbumId)
+                        : const DummyMusicCover(),
+                    title: Text(
+                      item.name,
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    subtitle: Text(item.owner),
+                    onTap: () async {
+                      // TODO: get playlist and save
+                      // final delegate = AnnixRouterDelegate.of(context);
+                      // delegate.to(name: '/playlist', arguments: item.id);
+                    },
+                  );
+                },
+                itemCount: result.value!.playlists?.length ?? 0,
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
